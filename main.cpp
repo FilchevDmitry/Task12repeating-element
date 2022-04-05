30d1d7199091281284ca9dab765dea253686a456
DmityFilchev <filchev_dmitry@mail.ru>
2022-04-05 12:31:42 +0300

    ok

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+ int numbers [15]={1,2,3,4,5,6,7,8,1,10,11,12,13,14,15};
+ int num=0,ni=0;
+    for (int i = 0; i < 15; ++i)
+    {
+        for (int j = 0; j < 15; ++j) {
+            if (numbers[i]==numbers[j])
+            {
+                ni++;
+                if(ni>1)
+                    num=numbers[j];
+            }
+        }
+        ni=0;
+    }
+    std::cout<<num;
+ return 0;
 }
